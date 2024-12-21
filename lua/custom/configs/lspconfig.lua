a7d45a85a1f42f57165b02be6daa2219adf7e2ad
Ryan Monahan <ryanaldo34@gmail.com>
2024-12-20 18:40:27 -0600

    Fix some go errors

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "clangd", "tsserver", "rust_analyzer", "csharp_ls", "terraformls",}
+local servers = { "html", "cssls", "clangd", "ts_ls", "rust_analyzer", "csharp_ls", "terraformls",}
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -42,7 +42,6 @@ lspconfig.pyright.setup({
 
 lspconfig.gopls.setup({
   on_attach = on_attach,
-  cmd = {"~/go/bin/gopls"}
 })
 
 local util = require 'lspconfig.util'
